bffa196ff8f4bed9f7015c420963ae032fe076c4
powerm1nt <nukaworks@nuka.works>
2022-05-27 16:14:20 +0200

    :sparkles: /etc directory making

--- entrypoint-ptero.sh
+++ entrypoint-ptero.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd /app
 
+# Make sure if the /etc directory is here on /home/container
+mkdir -p /home/container/etc
+
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
